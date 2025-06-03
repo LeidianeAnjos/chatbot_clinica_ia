34d47b9883d1013cd0933d1c7b7f998843d55649
LeidianeAnjos <leidiane.anjos@estudante.ifb.edu.br>
2025-06-03 16:14:03 -0300

    Atual

--- chatbot_base.py
+++ chatbot_base.py
@@ -14,6 +14,21 @@ perguntas_respostas = {
     "Quarta-feira": "Agendamento para quarta-feira confirmado!",
     "Quinta-feira": "Agendamento para quinta-feira confirmado!",
     "Sexta-feira": "Agendamento para sexta-feira confirmado!",
+    "1- Clínica Geral": " Agendado ",
+    "2- Ginecologia e Obstetrícia": "Ginecologia e Obstetrícia",
+    "3- Cardiologia": " Agendado Cardiologia",
+    "4- Dermatologia": " Agendado Dermatologia ",
+    "5- Ortopedia": " Agendado Ortopedia",
+    "6- Endocrinologia": " Agendado Endocrinologia",
+    "7- Psicologia": " Agendado Psicologia",
+    "8- Nutrição": " Agendado Nutrição",
+    "1- Amil": " Convênio Amil",
+    "2- Bradesco Saúde": " Convênio Bradesco Saúde",
+    "3- SulAmérica": " Convênio SulAmérica",
+    "4- Unimed": " Convênio Unimed",
+    "5- Hapvida": " Convênio Hapvida",
+    "6- Particular": " Convênio Particular",
+
 }
 
 # Teclado inicial com botões um abaixo do outro
@@ -38,13 +53,37 @@ def teclado_marcacao():
         resize_keyboard=True
     )
 
+def teclado_especialidade():
+    return ReplyKeyboardMarkup(
+        [["1- Clínica Geral"],
+         ["2- Ginecologia e Obstetrícia"],
+         ["3- Cardiologia"],
+         ["4- Dermatologia"],
+         ["5- Ortopedia"],
+         ["6- Endocrinologia"],
+         ["7- Psicologia"],
+         ["8- Nutrição"]],
+        resize_keyboard=True
+    )
+
+def teclado_convenio():
+    return ReplyKeyboardMarkup(
+        [["1- Amil"],
+         ["2- Bradesco Saúde"],
+         ["3- SulAmérica"],
+         ["4- Unimed"],
+         ["5- Hapvida"],
+         ["6- Particular"]],
+        resize_keyboard=True
+    )
+
 def responder(texto_usuario):
     texto_usuario = texto_usuario.strip()  # remover espaços em branco
 
-    # Se o usuário quer marcar consulta, mostrar opções de dias
+    # Se o usuário quer marcar consulta, mostrar especialidade
     if texto_usuario == "Quero marcar consulta":
         resposta = perguntas_respostas.get(texto_usuario)
-        return resposta, teclado_marcacao()
+        return resposta, teclado_especialidade()
 
     # Se o usuário selecionou um dia para consulta, confirmar e voltar para o teclado inicial
     if texto_usuario in ["Segunda-feira", "Terça-feira", "Quarta-feira"]:
